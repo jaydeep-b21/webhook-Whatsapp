ecb09efbfe0dca473da7d9747c3620ea94e9b1ac
Jaydeep Biswas <jaydeep.biswas21@gmail.com>
2024-12-24 18:31:55 +0530

    modified token

--- whatsapp_webhook/webhook/urls.py
+++ whatsapp_webhook/webhook/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('webhook/', views.webhook, name='webhook'),
     path('admin_interface/', views.admin_interface, name='admin_interface'),
     path('reply_to_user/', views.reply_to_user, name='reply_to_user'),
-
 ]
